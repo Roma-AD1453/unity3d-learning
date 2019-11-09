c6b44940fa4cf3bc6c7c60193c647ae3159ef161
Roma-AD1453 <55267031+Roma-AD1453@users.noreply.github.com>
2019-11-09 10:01:15 +0800

    Add files via upload

---
+++ Homework7/Assets/ColorChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    private ParticleSystem ps;
+    ParticleSystem.MainModule mainModule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        mainModule = ps.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 50), "Red"))
+        {
+            mainModule.startColor = Color.red;
+        }
+        if (GUI.Button(new Rect(100, 0, 100, 50), "Yellow"))
+        {
+            mainModule.startColor = Color.yellow;
+        }
+        if (GUI.Button(new Rect(200, 0, 100, 50), "Blue"))
+        {
+            mainModule.startColor = Color.blue;
+        }
+    }
+
+}
